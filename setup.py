1638e896ff95adb22df5b9ddd1d681e4577730f3
Rubiksel <ejderaksel@gmail.com>
2024-01-29 14:13:52 +0100

    Initializing the setup of the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='{game-of-life}',
+    version='1.0',
+    author='{Rubiksel}',
+    license='MIT',
+    long_description=open('README.md').read()
+)
